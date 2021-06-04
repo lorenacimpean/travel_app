3451b5da90e1f7b1014e66ae1d61fad86d439122
lorenacimpean <lorenacimpean@rodeapps.com>
2021-06-04 21:14:40 +0300

    replaced loading widget with circular progress indicator

--- lib/ui/widgets/loading_widget.dart
+++ lib/ui/widgets/loading_widget.dart
@@ -1,46 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/themes/app_colors.dart';
-import 'package:travel_app/themes/app_icons.dart';
 
-class LoadingWidget extends StatefulWidget {
-  LoadingWidget({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _LoadingWidgetState createState() => _LoadingWidgetState();
-}
-
-class _LoadingWidgetState extends State<LoadingWidget>
-    with TickerProviderStateMixin {
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 10),
-      vsync: this,
-    );
-  }
+class LoadingWidget extends StatelessWidget {
+  LoadingWidget({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    _controller.forward();
-    return Container(
-      color: AppColors.white,
-      child: RotationTransition(
-        turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
-        child: Image(
-          image: AppIcons.play_icon,
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    return Center(
+        child: CircularProgressIndicator(
+      backgroundColor: AppColors.primary,
+    ));
   }
 }
