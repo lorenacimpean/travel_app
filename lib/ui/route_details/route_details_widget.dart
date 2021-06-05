48d1e1225bec53973e9b02bb610692780701b52e
lorenacimpean <lorenacimpean@rodeapps.com>
2021-06-05 14:16:36 +0300

    added cached network image

--- lib/ui/route_details/route_details_widget.dart
+++ lib/ui/route_details/route_details_widget.dart
@@ -8,6 +8,7 @@ import 'package:travel_app/themes/app_dimen.dart';
 import 'package:travel_app/themes/app_icons.dart';
 import 'package:travel_app/themes/app_strings.dart';
 import 'package:travel_app/themes/app_text_styles.dart';
+import 'package:travel_app/ui/widgets/app_network_image.dart';
 import 'package:travel_app/ui/widgets/back_button_widget.dart';
 import 'package:travel_app/ui/widgets/blurred_button.dart';
 import 'package:travel_app/ui/widgets/pink_button.dart';
@@ -168,8 +169,7 @@ class RouteDetailsWidget extends StatelessWidget {
                     AppDimen.defaultCornerRadius,
                   ),
                 ),
-                child: Image.network(points[index]?.imageUrl ??
-                    "https://dummyimage.com/600x400/000/fff"),
+                child: AppNetworkImage(points[index]?.imageUrl),
               ),
             ),
           );
