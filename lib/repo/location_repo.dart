3dd4ecc63ebb4744488f235504dceec0acadd026
lorenacimpean <lorenacimpean@rodeapps.com>
2021-06-05 13:39:04 +0300

    replaced location with geolocation plugin because it's faster

--- lib/repo/location_repo.dart
+++ lib/repo/location_repo.dart
@@ -1,19 +1,16 @@
 import 'package:geocoding/geocoding.dart';
-import 'package:location/location.dart' as loc;
+import 'package:geolocator/geolocator.dart';
 import 'package:travel_app/models/location_model.dart';
 
 class LocationRepo {
-  final loc.Location _location;
-
-  LocationRepo({loc.Location location})
-      : _location = location ?? loc.Location();
-
-  Stream<LocationModel> getLocation() => _location.getLocation().asStream().map(
-        (data) => LocationModel(
-          latitude: data.latitude,
-          longitude: data.longitude,
-        ),
-      );
+  Stream<LocationModel> getLocation() {
+    return Geolocator.getLastKnownPosition().asStream().map(
+          (data) => LocationModel(
+            latitude: data.latitude,
+            longitude: data.longitude,
+          ),
+        );
+  }
 
   Stream<String> getAddressFromCoordinates(
     LocationModel location,
