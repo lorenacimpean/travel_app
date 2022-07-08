dfbdfea2d50ea8fad70a9ef9aaddb5559e2c2496
lorenacimpean <lorenacimpean@gmail.com>
2022-07-08 11:19:41 +0300

    update packages

--- lib/extensions/list_extensions.dart
+++ lib/extensions/list_extensions.dart
@@ -1,9 +1,9 @@
 extension ListExtensions<T> on List<T> {
-  int get hash {
-    if (this == null) return hashCode;
-    return map((e) => e.hashCode)?.reduce((res, e) => res ^ e.hashCode) ??
-        hashCode;
-  }
+  // int get hash {
+  //   if (this == null) return hashCode;
+  //   return map((e) => e.hashCode)?.reduce((res, e) => res ^ e.hashCode) ??
+  //       hashCode;
+  // }
 
   bool equals(List<T> other) {
     if (other == null && this == null) return true;
@@ -18,6 +18,7 @@ extension ListExtensions<T> on List<T> {
     return true;
   }
 }
+
 extension DynamicListExtension on List<dynamic> {
   List<T> toObjectList<T>(T Function(dynamic e) f) {
     if (this is! List || this == null) return null;
